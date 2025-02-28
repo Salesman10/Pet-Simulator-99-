7a2742c6b46e779da11129fc861dda2f472dff70
Salesman <samyberri7@gmail.com>
2025-02-28 22:51:11 +0100

    Simple.lua

---
+++ Simple.lua
@@ -0,0 +1,24 @@
+local lib = loadstring(game:HttpGet("https://raw.githubusercontent.com/bloodball/-back-ups-for-libs/main/bacon"))()
+local version, changelog = lib:GetInfo()
+local window = lib:CreateWindow("BY SALESMAN")
+local label = lib:CreateLabel(window, "Pets Simulator 99")
+local button = lib:CreateButton(window, "Titanic Hunter Script", function(script)
+
+     print(textBox.Text)
+end)
+
+local button = lib:CreateButton(window, "Huge Hunter Script", function(script)
+          
+     print(textBox.Text)
+end)
+
+local button = lib:CreateButton(window, "Scam Trade Script", function(script)
+         
+    print(textBox.Text)
+end)
+
+local button = lib:CreateButton(window, "Dupe Script", function(script)
+         
+    print(textBox.Text)
+end)
+
